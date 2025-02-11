8aabf6fe40bae05003341a15aa6cf49aca64e607
Harold Dennis R. Puno <borispuno@gmail.com>
2025-02-11 12:51:37 +0800

    Update main.js

--- app/screens/practice interface/easy/main.js
+++ app/screens/practice interface/easy/main.js
@@ -3,23 +3,23 @@ import { View, Image, StyleSheet, Dimensions, TouchableOpacity } from "react-nat
 
 const { width, height } = Dimensions.get("window");
 
-const backButton = require("./assets/Back.png");
-const shapeImg = require("./assets/Shape.png");
-const colorImg = require("./assets/Color.png");
-const numberImg = require("./assets/Number.png");
-const EasyFrameImg = require("./assets/EasyFrame.png");
+const backButton = require("../../../assets/Back.png");
+const shapeImg = require("../../../assets/Shape.png");
+const colorImg = require("../../../assets/Color.png");
+const numberImg = require("../../../assets/Number.png");
+const EasyFrameImg = require("../../../assets/EasyFrame.png");
 const shapes = {
-  ellipse5: require("./assets/Ellipse 5.png"),
-  ellipse7: require("./assets/Ellipse 7.png"),
-  rect10: require("./assets/Rectangle 10.png"),
-  rect11: require("./assets/Rectangle 11.png"),
-  rect12: require("./assets/Rectangle 12.png"),
-  rect21: require("./assets/Rectangle 21.png"),
-  rect23: require("./assets/Rectangle 23.png"),
-  rect24: require("./assets/Rectangle 24.png"),
-  rect25: require("./assets/Rectangle 25.png"),
-  countdown: require("./assets/countdown.png"),
-  copyright: require("./assets/copyright.png"),
+  shape1: require("../../../assets/shape1.png"),
+  shape2: require("../../../assets/shape2.png"),
+  shape3: require("../../../assets/shape3.png"),
+  shape4: require("../../../assets/shape4.png"),
+  shape5: require("../../../assets/shape5.png"),
+  shape6: require("../../../assets/shape6.png"),
+  shape7: require("../../../assets/shape7.png"),
+  shape8: require("../../../assets/shape8.png"),
+  shape9: require("../../../assets/shape9.png"),
+  countdown: require("../../../assets/countdown.png"),
+  copyright: require("../../../assets/copyright.png"),
 };
 
 const DifficultySelection = () => {
@@ -109,15 +109,15 @@ const styles = StyleSheet.create({
     position: "absolute",
     resizeMode: "contain",
   },
-  ellipse5: { top: 590, left: 100, width: 120, height: 150, zIndex: 2 },
-  ellipse7: { top: 170, right: -70, width: 200, height: 245 },
-  rect10: { bottom: 0, left: 1080, width: 200, height: 140 },
-  rect11: { top: 90, right: 990, width: 500, height: 500 },
-  rect12: { bottom: 310, left: 1, width: 110, height: 180 },
-  rect21: { top: 650, left: 5, width: 140, height: 140 },
-  rect23: { bottom: 655, right: 690, width: 130, height: 150 },
-  rect24: { bottom: 45, right: 100, width: 140, height: 90 },
-  rect25: { bottom: 670, right: 800, width: 100, height: 140 },
+  shape1: { top: 590, left: 100, width: 120, height: 150, zIndex: 2 },
+  shape2: { top: 170, right: -70, width: 200, height: 245 },
+  shape3: { bottom: 0, left: 1080, width: 200, height: 140 },
+  shape4: { top: 90, right: 990, width: 500, height: 500 },
+  shape5: { bottom: 310, left: 1, width: 110, height: 180 },
+  shape6: { top: 650, left: 5, width: 140, height: 140 },
+  shape7: { bottom: 655, right: 690, width: 130, height: 150 },
+  shape8: { bottom: 45, right: 100, width: 140, height: 90 },
+  shape9: { bottom: 670, right: 800, width: 100, height: 140 },
   countdown: { top: -20, right: 90, width: 100, height: 140 },
   copyright: { bottom: 20, alignSelf: "center", width: 200, height: 50 },
   EasyFrame: { top: 120, alignSelf: "center", width: 250, height: 100, resizeMode: "contain", zIndex: 2 },
